5a37eca045ca1c3ac4d0fc382b32188906f9e038
trishmarie002-art <trishmarie002@gmail.com>
2026-08-06 17:59:32 -0500

    Use photos from shared Google Photos album

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -50,7 +50,7 @@ export const AboutPage: React.FC<AboutPageProps> = ({ onOpenAirbnb }) => {
           <div className="lg:col-span-5 relative">
             <div className="rounded-2xl overflow-hidden border border-amber-400/30 shadow-2xl aspect-square relative">
               <img
-                src="https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTA0MDY1NjkwNTgzNzk3OTgzOA%3D%3D/original/de20e643-2eab-4685-b377-39519e624764.jpeg"
+                src="https://lh3.googleusercontent.com/pw/AP1GczPy0d43HEpcsXcNXE63fVH_GayjLjJPodXncTe6VNVo4DaRj_47sktozXglTWFS_LLTGWhLZMLu9qBxJUtiqWYM8_PZfxfKfRCa_3_1sgFhSawjf04=w1600"
                 alt="Cliff House at Canyon Lake waterfront property"
                 className="w-full h-full object-cover"
                 referrerPolicy="no-referrer"

b0a9fc3a3d10c249a66139ec167b8be46a51ab77
trishmarie002-art <trishmarie002@gmail.com>
2026-08-06 17:51:08 -0500

    Fix Airbnb image URLs

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,8 +10,8 @@ interface HeroProps {
 
 export const Hero: React.FC<HeroProps> = ({ onOpenAirbnb, ambiance }) => {
   const bgImage = ambiance === 'sunset'
-    ? 'https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTA0MDY1NjkwNTgzNzk3OTgzOA%3D%3D/original/de20e643-2eab-4685-b377-39519e624764.jpeg?im_w=1600'
-    : 'https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTA0MDY1NjkwNTgzNzk3OTgzOA%3D%3D/original/a73c509d-2758-4898-914c-65fa01cf3459.jpeg?im_w=1600';
+    ? 'https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTA0MDY1NjkwNTgzNzk3OTgzOA%3D%3D/original/de20e643-2eab-4685-b377-39519e624764.jpeg'
+    : 'https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTA0MDY1NjkwNTgzNzk3OTgzOA%3D%3D/original/a73c509d-2758-4898-914c-65fa01cf3459.jpeg';
 
   return (
     <section className="relative min-h-screen flex items-center justify-center overflow-hidden pt-20 pb-16">
